7e2f0a6ddf7fa821a22e4fe20cb94037e2e09e62
Kelvin Gil <gilkelvin2@gmail.com>
2024-01-09 14:55:21 -0500

    wrote pseudocode for js file

---
+++ index.js
@@ -0,0 +1,28 @@
+// create the character chards for the page 
+fetch('https://rickandmortyapi.com/api/character',createCharacterCard(character))
+
+function createCharacterCard(character){
+    
+}
+
+
+
+
+// add a event listener that listens for a dble click and when a dble click event is triggered an alert is supossed to pop up and say this character has been likeed( this being the targeted character card )
+// function for event listener 
+
+
+
+//create a submit event listner for every cahracter card that filters the and tries to finds the specific they were in 
+// or have a submt event listner that filter the characters by episode and displays the first 5-10 on the screen 
+//fucntion for event listener 
+
+
+
+// on every card there will be a list showing the episodes they were in and will have a scroll event listener 
+//function for event listener 
+
+
+
+//add buttons and label them properly, name them accordingly 
+// the buttons should go into the card as a click event listner 
